77d21e254f7d2f177853bd5fb90475994ccda0b1
eminbasbayan <emin.basbayan@icloud.com>
2026-06-15 14:18:22 +0300

    product card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
+import ProductCard from "./ProductCard";
 
 function App() {
-  const [state, setState] =  useState("Ahmet")
-  /* let customerName = 'Ahmet'; */
-
-  function customerUpdate() {
-    /* customerName = 'Emre'; */
-    setState("Emre")
-  }
+ 
 
   return (
     <div>
-      <h1>Müşteri ismi: {state}</h1>
-      <button onClick={customerUpdate}>İsmi Güncelle</button>
+      <ProductCard />
     </div>
   );
 }
---
+++ src/ProductCard.jsx
@@ -0,0 +1,11 @@
+function ProductCard() {
+  return (
+    <div className="product-card">
+      <img className="product-image" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png" />
+      <b className="product-title">Çanta</b>
+      <span className="product-price">200₺</span>
+    </div>
+  );
+}
+
+export default ProductCard
