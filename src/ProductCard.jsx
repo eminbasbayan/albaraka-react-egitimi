de64183d32e7775dc0fa171fe9bb90c065696bbf
eminbasbayan <emin.basbayan@icloud.com>
2026-06-15 14:21:57 +0300

    inline css

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -1,11 +1,21 @@
 function ProductCard() {
+  const productCardStyle = {
+    backgroundColor: 'red',
+  };
+
   return (
-    <div className="product-card">
-      <img className="product-image" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png" />
+    <div className="product-card" style={productCardStyle}>
+      <img
+        className="product-image"
+        src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
+        style={{
+          width: '200px',
+        }}
+      />
       <b className="product-title">Çanta</b>
       <span className="product-price">200₺</span>
     </div>
   );
 }
 
-export default ProductCard
+export default ProductCard;
