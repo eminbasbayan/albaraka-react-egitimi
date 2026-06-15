38f70ed94742401b1ab71810b4b652a28823deae
eminbasbayan <emin.basbayan@icloud.com>
2026-06-15 14:00:23 +0300

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-function App(){
-  return <h1>Hello React!</h1>
+import { useState } from "react";
+
+function App() {
+  const [state, setState] =  useState("Ahmet")
+  /* let customerName = 'Ahmet'; */
+
+  function customerUpdate() {
+    /* customerName = 'Emre'; */
+    setState("Emre")
+  }
+
+  return (
+    <div>
+      <h1>Müşteri ismi: {state}</h1>
+      <button onClick={customerUpdate}>İsmi Güncelle</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
