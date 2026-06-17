e374ce01e11612b85d8b02477133e13c835de03b
eminbasbayan <emin.basbayan@icloud.com>
2026-06-17 15:27:12 +0300

    localstorage kullanımı

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,15 +1,23 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const CartContext = createContext();
 
 function CartProvider({ children }) {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(
+    localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+  );
 
   console.log(cartItems);
 
   function addToCart(product) {
     setCartItems((prev) => [...prev, product]);
   }
+
+  useEffect(() => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
 
   return (
     <CartContext.Provider
