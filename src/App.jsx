809cd52014f39f7fec23c507689101047628631d
eminbasbayan <emin.basbayan@icloud.com>
2026-06-18 16:08:42 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { RouterProvider } from 'react-router';
 import router from './routes/routes';
 import { ToastContainer } from 'react-toastify';
-import Counter from './components/Counter';
 
 function App() {
 
   return (
     <div className="root-component">
-      <Counter />
       <ToastContainer />
       <RouterProvider router={router} />
     </div>
